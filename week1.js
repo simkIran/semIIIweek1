58abb893c0c0647cbacf8ee8b59beb26f5db3dd3
Kirandeep Kaur <741453@student.tor.loc>
2020-01-08 15:20:44 -0500

    initial comment

---
+++ week1.js
@@ -0,0 +1,19 @@
+/*for(i=1;i<101;i++)
+{
+console.log(i);
+}
+
+i=1;
+while(i<101)
+{
+    console.log(i);
+    i++;
+}*/
+
+i=100; sum=0;
+for(;i>0;)
+{
+    sum+=i;
+    i=i-6;
+    console.log(i);
+}
